e92ccb353a31e81531a23a86e55148133bb41398
Tri-C Faculty-Staff <Tri-C Faculty-Staff@STUDENT24>
2017-06-14 14:46:13 -0400

    Added car1 and car2

---
+++ Day3OOP/Day3OOP/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3OOP
+{
+    class Car
+    {
+        public string make;
+        public string model;
+        public string color;
+        public int year;
+        public float MSRP;
+        public int VIN;
+
+        public float sellCar(string person, float salesPrice)
+        {
+            return this.MSRP / salesPrice * 1000;
+        }
+    }
+}
--- Day3OOP/Day3OOP/Program.cs
+++ Day3OOP/Day3OOP/Program.cs
@@ -10,6 +10,19 @@ namespace Day3OOP
     {
         static void Main(string[] args)
         {
+            Car car1 = new Car();
+            car1.make = "Ford";
+            car1.model = "Focus";
+            car1.color = "Red";
+            car1.year = 2010;
+
+            Car car2 = new Car();
+            car2.make = "Kia";
+            car2.model = "Spectra";
+            car2.color = "Black";
+            car2.year = 2007;
+
+
         }
     }
 }
